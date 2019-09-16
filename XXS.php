e82eba513afc27511ba3121e8b7766dc9a49c621
PabloMaster2 <55392265+PabloMaster2@users.noreply.github.com>
2019-09-16 14:25:33 +0200

    Add files via upload

---
+++ XXS.php
@@ -0,0 +1,9 @@
+<?php
+
+IF(isset($_POST['dane']))
+{
+    echo 'Wpisałeś następujące dane:<br>';
+    echo $_POST['dane'];
+    // zapisać dane itd.
+}
+echo '<form action="bug.php" method="post"><input type="submit" value="wyślij"><input type="text" name="dane" size="14"></form>';
